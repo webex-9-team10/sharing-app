8439b40cf99f074ab1e352262e277e5a72304dba
YutoFukuyama <fukuyuu.2000704@gmail.com>
2021-03-09 13:33:46 +0900

    firebase導入したよ

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3S8WiOHBUEVmi38BbRxCW1jt3kke2dTM",
+    authDomain: "sharing-app-68d4b.firebaseapp.com",
+    projectId: "sharing-app-68d4b",
+    storageBucket: "sharing-app-68d4b.appspot.com",
+    messagingSenderId: "334785359736",
+    appId: "1:334785359736:web:2a5b7276d4917d35d0a28d",
+    measurementId: "G-ZFH0TE2122"
+};
+
+firebase.initializeApp(firebaseConfig);
