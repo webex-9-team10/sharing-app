279fa5f19383cae4e9e4cc41edeedd6feceb200b
Noahmartin-hub <noanoa_n.y@keio.jp>
2021-03-24 15:17:36 +0900

    背景少しいじってみた

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #818181;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
 }
 
 #nav {
--- src/components/SubmitForm.vue
+++ src/components/SubmitForm.vue
@@ -1,43 +1,68 @@
 <template>
-  <div class="form__wrapper">
-    {{ position }}
-    <div>
-      <label
-        >ファイルを選択
-        <input type="file" />
-      </label>
-      <span id="file_name"
-        >sample.csv <span class="reset_file_ico">×</span></span
-      >
-      <p id="error">csv ファイルのみアップロード可能です</p>
+  <section id="contact">
+    <h1 class="section-header">Let's post!</h1>
+    <div class="form__wrapper">
+      <div class="contact-wrapper">
+        <form id="contact-form" class="form-horizontal" role="form">
+          <!-- 緯度経度の情報持ってくる -->
+          {{ position }}
+          <!-- ファイル選択画面 -->
+          <div class="form-group">
+            <div class="form-control">
+              <!-- ファイルを選択 -->
+              <div class="chooseFile">
+                <label
+                  >ファイルを選択
+                  <input type="file" />
+                </label>
+              </div>
+              <!-- ファイルのみアップロード可能です -->
+              <div class="onlyfile">
+                <span id="file_name"
+                  >sample.csv <span class="reset_file_ico">×</span></span
+                >
+                <p id="error">csv ファイルのみアップロード可能です</p>
+              </div>
+            </div>
+            <!-- ジャンル選択画面 -->
+            <div class="form-control">
+              <select v-model="genre">
+                <option disabled value="">ジャンルの選択</option>
+                <option>food</option>
+                <option>museum</option>
+                <option>date</option>
+                <option>chill</option>
+                <option>cafe</option>
+              </select>
+            </div>
+            <!-- タイトル選択画面 -->
+            <div class="chooseTitle">
+              <input type="text" v-model="title" placeholder="title" />
+            </div>
+            <!-- キャプション画面 -->
+            <div class="caption">
+              <textarea
+                class="form-control"
+                v-model="text"
+                placeholder="キャプションを書く"
+              />
+            </div>
+            <!-- 投稿ボタン -->
+            <div class="form__buttons">
+              <button v-on:click="postTweet" class="form__submit-button">
+                投稿
+              </button>
+            </div>
+            <div v-for="tweet in tweets" :key="tweet.id">
+              <router-link :to="{ name: 'Show', params: { postid: tweet.id } }">
+                {{ tweet.text }}
+              </router-link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-    <select v-model="genre">
-      <option disabled value="">ジャンルの選択</option>
-      <option>food</option>
-      <option>museum</option>
-      <option>date</option>
-      <option>chill</option>
-      <option>cafe</option>
-    </select>
-    <div>
-      <input type="text" v-model="title" placeholder="title" />
-    </div>
-    <textarea
-      class="form__textarea"
-      v-model="text"
-      placeholder="キャプションを書く"
-    />
-    <div class="form__buttons">
-      <button v-on:click="postTweet" class="form__submit-button">
-        投稿
-      </button>
-    </div>
-    <div v-for="tweet in tweets" :key="tweet.id">
-      <router-link :to="{ name: 'Show', params: { postid: tweet.id } }">
-        {{ tweet.text }}
-      </router-link>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -99,7 +124,94 @@ export default {
 </script>
 
 <style scoped>
-.form__wrapper {
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
+
+#contact {
+  width: 100%;
+  height: 100vh;
+  background-color: #000;
+  overflow: hidden;
+}
+
+.section-header {
+  text-align: center;
+  margin: 0 auto;
+  padding: 40px 0;
+  font: 300 60px "Nunito Sans", sans-serif;
+  letter-spacing: 6px;
+  color: #fff;
+}
+
+.contact-wrapper {
+  margin: 0 auto;
+  padding-top: 20px;
+  position: relative;
+  max-width: 800px;
+  font-family: sans-serif;
+}
+
+/* Begin Left Contact Page */
+.form-horizontal {
+  text-align: center;
+  flex-direction: column;
+  color: #fff;
+  font-family: "Lato";
+}
+
+.chooseFile {
+  position: relative;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+textarea.form-control {
+  width: 90%;
+
+  color: #fff;
+  letter-spacing: 1px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  height: 50px;
+  padding: 12px 12px;
+  font-size: 14px;
+}
+
+.caption {
+  padding: 10px 18px;
+}
+
+.send-button {
+  margin-top: 15px;
+  height: 34px;
+  width: 400px;
+  overflow: hidden;
+  transition: all 0.2s ease-in-out;
+}
+
+.button {
+  width: 400px;
+  height: auto;
+  transition: all 0.2s ease-in-out;
+}
+
+.send-text {
+  display: block;
+  margin-top: 10px;
+  font: 700 12px "Lato", sans-serif;
+  letter-spacing: 2px;
+}
+
+.button:hover {
+  transform: translate3d(0px, -29px, 0px);
+}
+
+/* 以下、既存 */
+/* .form__wrapper {
   font-family: "Nunito Sans", sans-serif;
   font-weight: 400;
   font-style: normal;
@@ -108,8 +220,8 @@ export default {
   line-height: 1.65;
   box-sizing: border-box !important;
   -webkit-font-smoothing: antialiased;
-}
-.form__textarea {
+} */
+/* .form__textarea {
   font-family: "Nunito Sans", sans-serif;
   width: 100%;
   height: calc(1.3rem * 3 + 0.5rem * 2);
@@ -129,7 +241,7 @@ export default {
 }
 .form__textarea:focus {
   outline: none;
-}
+} */
 /* 投稿ボタン */
 .form__buttons {
   font-family: "Nunito Sans", sans-serif;
@@ -138,9 +250,9 @@ export default {
   display: inline-block;
   max-width: 180px;
   text-align: left;
-  border: 2px solid #9ec34b;
+  border: 2px solid white;
   font-size: 16px;
-  color: #9ec34b;
+  color: white;
   text-decoration: none;
   font-weight: bold;
   padding: 8px 16px;
@@ -183,6 +295,6 @@ label input {
 }
 #error {
   font-family: "Nunito Sans", sans-serif;
-  color: red;
+  color: #fff;
 }
 </style>
